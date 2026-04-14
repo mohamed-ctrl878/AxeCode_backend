417f19693f79ff1fff4b4ea95900b281982743ba
mohamed mahmoud <mamed3628@gmail.com>
2026-04-14 10:41:17 +0200

    fix: jwtSecret and env configuration

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,9 @@
-module.exports = {
+module.exports = ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET'),
+    },
+  },
   // ...
   mcp: {
     enabled: true,
